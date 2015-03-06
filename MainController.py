b39acc935d57e65e826398e8195378a31f005b60
Lars de Bruijn <larsdebruijn12@gmail.com>
2015-03-06 15:23:54 +0100

    Added documentation to MainController

--- MainController.py
+++ MainController.py
@@ -1,6 +1,10 @@
 from PredictionController import PredictionController
 
 class MainController(object):
+	""" The MainController is the top level Class and the highest layer of the
+		application. This Class initialises the entire application and makes sure
+		everythin is running.
+	"""
 
 	def __init__(self):
 		self.predictionController = PredictionController()
